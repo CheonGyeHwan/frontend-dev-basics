678dfc5bfd833543117fb9673e18cec4e63f071e
CheonGyeHwan <cgh5531@naver.com>
2022-02-17 15:06:25 +0900

    javascript #1

---
+++ javascript-practices/basics/ex02.js
@@ -0,0 +1,48 @@
+/*
+변수와 데이터 타입
+[기본 타입]
+  - undefined
+  - number
+  - string
+  - boolean
+
+[객체] 
+  - object
+    - 1. new 생성자함수() 사용해서 생성
+    - 2.
+    - 3.
+    - 4.
+
+  - function
+
+*/
+
+console.log("=======[기본 타입]========")
+var u = undefined; // var u; ==> 선언과 정의의 구분이 없다.
+var i = 10;
+var s = "hello world";
+var b = true;
+
+console.log("u : " + typeof(u));
+console.log("i : " + typeof(i));
+console.log("s : " + typeof(s));
+console.log("b : " + typeof(b));
+
+console.log("=======[객체1 : object type]========")
+
+var i2 = new Number(1);
+var s2 = new String("Hello World");
+var b2 = new Boolean(false);
+var o = new Object();
+var a = new Array();                  // 배열도 객체
+
+console.log("i2 : " + typeof(i2));
+console.log("s2 : " + typeof(s2));
+console.log("b2 : " + typeof(b2));
+console.log("o : " + typeof(o));
+console.log("a : " + typeof(a));
+
+
+
+
+console.log("=======[객체2 : function type]========")
