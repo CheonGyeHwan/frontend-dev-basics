e6551f137771f44d6482fba44c08e21a432f5403
CheonGyeHwan <cgh5531@naver.com>
2022-02-21 14:47:35 +0900

    js practices #8

--- javascript-practices/basics/ex09.js
+++ javascript-practices/basics/ex09.js
@@ -12,8 +12,15 @@ MyObject.prototype.info = function() {
     console.log(this.name + " , " + this.age + " , " + this.school + " , " + this.course);
 };
 
-// MyObject1 객체 생성
+// MyObject 객체 생성1
 var o1 = new MyObject("둘리", 10);
 o1.info();
 o1.school = "BIT ACADEMY";
 o1.info();
+
+// MyObject1 객체 생성2
+var o2 = new MyObject("마이콜", 15);
+o2.info = function() {
+    console.log("비밀");
+}
+o2.info();
