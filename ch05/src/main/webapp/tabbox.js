c8f0e72184d7703ee21f43e633af5a5358cf4383
CheonGyeHwan <cgh5531@naver.com>
2022-02-23 14:52:23 +0900

    DOM API #final

---
+++ ch05/src/main/webapp/tabbox.js
@@ -0,0 +1,26 @@
+var tabBox = {
+	_onWindowload : function() {
+		var divTabBox = document.getElementsByClassName("tab-box")[0];
+		var ul = divTabBox.childNodes[1];
+		
+		var liTabs = ul.getElementsByTagName("li");
+
+		for (var i = 0; i < liTabs.length; i++) {
+			liTabs[i].addEventListener("click", this._onTabClicked);
+		}
+	},
+	
+	_onTabClicked : function() {
+		// unselected
+		var lisSelected = document.getElementsByClassName("selected");
+		(lisSelected.length === 1) && (lisSelected[0].className = "");
+		
+		// select
+		this.className = "selected";	
+	},
+	
+	init : function() {
+		window.addEventListener("load", this._onWindowload.bind(this));
+	}
+
+}
